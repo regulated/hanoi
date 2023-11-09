a90fc3d44665087cba77ab12aab01a0a3ba44194
regulated <eschem58@gmail.com>
2023-11-08 19:57:20 -0600

    relative sizing

--- src/styles/home-styles.ts
+++ src/styles/home-styles.ts
@@ -2,9 +2,9 @@
 export const appContainer =
   "container bg-zinc-800 flex flex-col items-center justify-center min-h-screen min-w-full";
 export const title =
-  "text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-400 mb-12";
+  "text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-400 sm:mb-12 mb-2 ";
 export const purple = "text-purple-300";
 export const body = "text-2xl text-gray-700";
 export const grid = "grid gap-12 items-center justify-center md:grid-cols-3";
 export const input = "form-input w-24 py-0 px-4 m-0 rounded-xl bg-gray-200";
-export const buttons = "align-center grid grid-cols-2 grid-rows-2 justify-center items-center gap-4 mt-12"
+export const buttons = "align-center grid grid-cols-2 grid-rows-2 justify-center items-center gap-4 sm:mt-12"
